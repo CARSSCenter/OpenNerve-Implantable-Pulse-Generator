f8654fecc1df002897834c651066459075fceeb1
axbaldwin1 <124087207+axbaldwin1@users.noreply.github.com>
2025-05-21 13:57:51 -0700

    Add files via upload

---
+++ Windows-Controller/controller/Program.cs
@@ -0,0 +1,12 @@
+namespace controller
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
